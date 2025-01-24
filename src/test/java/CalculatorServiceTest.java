e5f8b803a7e6f84ca0db6075e2f46d0d2d2b0fdc
itllsendamsg <11300913+itllsendamsg@users.noreply.github.com>
2025-01-24 12:59:06 +0100

    24 1 25 / The latest project files cleaning process

--- src/test/java/CalculatorServiceTest.java
+++ src/test/java/CalculatorServiceTest.java
@@ -1,11 +1,13 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigInteger;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorServiceTest {
 
@@ -27,4 +29,17 @@ class CalculatorServiceTest {
         // then
         assertThat(result).isEqualTo(BigInteger.valueOf(24));
     }
+
+    @ParameterizedTest
+    @MethodSource("factorialTest")
+    void shouldReturnCorrectValue(int n, BigInteger result) {
+
+    }
+
+    static Stream<Arguments>factorialTest(){
+        return Stream.of(
+            Arguments.of()
+        );
+    }
+
 }
