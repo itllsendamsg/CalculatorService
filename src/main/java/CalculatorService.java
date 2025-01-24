010da58548a8f745a1749a08e05618e8058ddcd9
itllsendamsg <11300913+itllsendamsg@users.noreply.github.com>
2025-01-24 14:35:15 +0100

    24 1 25 / The latest project files cleaning process

--- src/main/java/CalculatorService.java
+++ src/main/java/CalculatorService.java
@@ -3,7 +3,11 @@ import java.math.BigInteger;
 public class CalculatorService {
 
     public BigInteger factorial(int n) {
-        return null;
+        if (n < 0) {
+            return BigInteger.ZERO;
+        } else if (n == 0) {
+            return BigInteger.ONE;
+        }
+        return BigInteger.valueOf(n).multiply(factorial(n - 1));
     }
-
 }
--- src/test/java/CalculatorServiceTest.java
+++ src/test/java/CalculatorServiceTest.java
@@ -32,13 +32,26 @@ class CalculatorServiceTest {
 
     @ParameterizedTest
     @MethodSource("factorialTest")
-    void shouldReturnCorrectValue(int n, BigInteger result) {
+    void shouldReturnCorrectValue(int n, BigInteger expectedResult) {
+        // given
 
+        // when
+        BigInteger result = calculatorService.factorial(n);
+
+        // then
+        assertThat(result).isEqualTo(expectedResult);
     }
 
-    static Stream<Arguments>factorialTest(){
+    static Stream<Arguments> factorialTest() {
         return Stream.of(
-            Arguments.of()
+                Arguments.of(-1, BigInteger.valueOf(0)),
+                Arguments.of(0, BigInteger.valueOf(1)),
+                Arguments.of(1, BigInteger.valueOf(1)),
+                Arguments.of(2, BigInteger.valueOf(2)),
+                Arguments.of(3, BigInteger.valueOf(6)),
+                Arguments.of(4, BigInteger.valueOf(24)),
+                Arguments.of(111, new BigInteger("1762952551090244663872161047107075788761409536026565516041574063347346955087248316436555574598462315773196047662837978913145847497199871623320096254145331200000000000000000000000000"))
+
         );
     }
 
