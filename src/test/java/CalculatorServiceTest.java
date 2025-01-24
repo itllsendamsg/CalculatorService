fad880d4c90864bdbb00374d72fc8838f114de59
itllsendamsg <11300913+itllsendamsg@users.noreply.github.com>
2025-01-24 12:02:33 +0100

    24 1 25 / The latest project files cleaning process

--- src/test/java/CalculatorServiceTest.java
+++ src/test/java/CalculatorServiceTest.java
@@ -5,8 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorServiceTest {
 
+    private CalculatorService calculatorService;
+
     @BeforeEach
     void setUp() {
+        calculatorService = new CalculatorService();
     }
 
     @Test
