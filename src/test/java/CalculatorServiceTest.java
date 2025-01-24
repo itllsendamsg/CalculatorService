28f20bbb8de81914d46e984a4035f365db0d89e5
itllsendamsg <11300913+itllsendamsg@users.noreply.github.com>
2025-01-24 12:37:01 +0100

    24 1 25 / The latest project files cleaning process

--- src/test/java/CalculatorServiceTest.java
+++ src/test/java/CalculatorServiceTest.java
@@ -1,6 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorServiceTest {
@@ -13,6 +17,14 @@ class CalculatorServiceTest {
     }
 
     @Test
-    void factorial() {
+    void shouldReturnCorrectValueForSmallPositiveNumbers() {
+        // given
+        int n = 4;
+
+        // when
+        BigInteger result = calculatorService.factorial(n);
+
+        // then
+        assertThat(result).isEqualTo(BigInteger.valueOf(24));
     }
 }
